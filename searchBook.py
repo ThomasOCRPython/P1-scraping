0b3462b5419280d316a7bd7dd731fb6ee637e5e5
Thomas2020 <thomas.duval2020@campus-eni.fr>
2021-05-20 00:33:03 +0200

    fonction qui scrape la description reste le pb encodage

--- searchBook.py
+++ searchBook.py
@@ -8,21 +8,32 @@ def get_category(soup):
     categoryReplace=category.replace('\n','')
     return categoryReplace
 
-def get_review_rating(url):
+def get_reviewRating(soup):
+  
+    #p=soup.find('div',{'class':'col-sm-6 product_main'}).find('p').find_next().find_next().find_next()
+    p= soup.find('div',{'class':'col-sm-6 product_main'}).find("p",class_="star-rating")
+    rating=str(p["class"])
+    star=rating[15:-1]
+    starRating=eval(star)
+    return starRating
+
+def get_description(url):
     URL=url
     livre= requests.get(URL)
 
     if livre.ok: 
         soup= BeautifulSoup(livre.text,"html.parser")
-        #p=soup.find('div',{'class':'col-sm-6 product_main'}).find('p').find_next().find_next().find_next()
-        p=soup.find('div',{'class':'col-sm-6 product_main'}).find("p",class_="star-rating")
-        rating=str(p["class"])
-        starRating=rating[15:-1]
-        print ("coucou: "+ starRating)
+
+        paragraphs=soup.findAll('p')
+
+        p=paragraphs[3].encode('utf-8')
+        
+        print(p)    
 
 
 
 def scrapBook(url):
+
     
     URL=url
     livre= requests.get(URL)
@@ -33,8 +44,17 @@ def scrapBook(url):
         titre= soup.find('div',{'class':'col-sm-6 product_main'}).find('h1')
 
         #Description
-        #productDescription=soup.findAll('div',{'class':'product_description'}).find_next_sibling("p")
-        #print(productDescription)
+        #productDescription=soup.find('div',class_='product_description').find_next().find_next().find_next()
+        # paragraphs=soup.findAll('p')
+        # linkp=[]
+        # for paragraph in paragraphs:
+        #     p=paragraph.text
+        #     linkp.append(p)
+        
+
+        
+        
+        
         ###############################################################################
 
         #Upc, price tax including, price taxe excluding,Type , Tax, nb available, stock
@@ -44,8 +64,7 @@ def scrapBook(url):
         image=soup.find('div',{'class':'item active'}).find('img')
         image_url=image['src']
 
-        #review_rating
-        ###############################################################################
+    
 
         book={}
         book['product_page_url']=URL
@@ -56,14 +75,15 @@ def scrapBook(url):
         book['number_available']=tds[5].text
         #book['product_description']=productDescription
         book['category']= get_category(soup)
-        #book['review_rating']=image_url
+        book['review_rating']= get_reviewRating(soup)
         book['image_url']=image_url
         book['Tax']=tds[4].text
 
-        print(book)
+        #print(book)
 
 #scrapBook("http://books.toscrape.com/catalogue/security_925/index.html")
-get_review_rating("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+get_description("http://books.toscrape.com/catalogue/security_925/index.html")
+
 
      
     
