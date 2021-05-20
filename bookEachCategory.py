20c0b642f95cc55b3ae5db031578d2f56516dca2
Thomas2020 <thomas.duval2020@campus-eni.fr>
2021-05-20 18:44:57 +0200

    test gitignore

--- bookEachCategory.py
+++ bookEachCategory.py
@@ -26,13 +26,13 @@ def searchBookCategory(url):
     # return livreUrl 
       
     with open('./data/Christian/Christian.csv','w',newline='') as f:
-        # for book in books:
-        #      f.write('{},{},{},{},{},{},{},{},{},{},{}\n'.format(book['product_page_url'],book['universal_ product_code'],book['title'],book['price_including_tax'],book['price_excluding_tax'],book['number_available'],book['product_description'],book['category'],book['review_rating'],book['image_url'],book['Tax'])) 
+        
          fieldnames=['product_page_url','universal_ product_code','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url','Tax']
 
-         writer = csv.DictWriter(f,delimiter=" ",fieldnames=fieldnames,restval='', extrasaction='ignore')
+         writer = csv.DictWriter(f,delimiter=" ",fieldnames=fieldnames)
          writer.writeheader()
-         writer.writerows(books)
+         for book in books:
+              writer.writerows(book)
         
         
         
